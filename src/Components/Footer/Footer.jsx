c5d9137f1fc0e3ed96e0eb1cb6fa00f49028b275
anand <anandsawarn11@gmail.com>
2025-01-28 15:37:15 +0530

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { MdOutlineEmail } from "react-icons/md";
+import { CiLinkedin } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div
+      id="Footer"
+      className="flex justify-around bg-[#465697] text-white p-10 md:p-12 items-center"
+    >
+      <div>
+        <h1 className="text-2xl md:text-6xl font-bold">Contact</h1>
+        <h3 className="text-sm md:text-2xl font-normal">
+          Feel Free To reach out!
+        </h3>
+      </div>
+
+      <ul className="text-sm md:text-xl">
+        <li className="flex gap-1 items-center">
+          <MdOutlineEmail size={20} />
+          <a href="mailto:mailsforanandkumar@gmail.com">
+            mailsforanandkumar@gmail.com
+          </a>
+        </li>
+        <li className="flex gap-1 items-center">
+          <CiLinkedin />
+          <a href="https://www.linkedin.com/in/anand-kumar-404959237" target="_blank" rel="noopener noreferrer">
+            www.linkedin.com/in/anand-kumar-404959237
+          </a>
+        </li>
+        <li className="flex gap-1 items-center">
+          <FaGithub />
+          <a href="https://github.com/username" target="_blank" rel="noopener noreferrer">
+            github.com/username
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
